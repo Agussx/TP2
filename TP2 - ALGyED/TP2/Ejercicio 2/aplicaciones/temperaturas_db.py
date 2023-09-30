ebe6cb0a5a3affed899a0e288b6013ef621023c1
Agussx <127306458+Agussx@users.noreply.github.com>
2023-09-30 11:10:53 -0300

    Add files via upload
    cambios 30/09/23

---
+++ TP2 - ALGyED/TP2/Ejercicio 2/aplicaciones/temperaturas_db.py
@@ -0,0 +1,19 @@
+class Temperatura_db: 
+    def __init__(self):
+        pass 
+    def guardar_temperatura(temperatura,fecha):
+        pass
+    def devolver_temperatura(fecha): 
+     pass 
+    def max_temp_rango(fecha1,fecha2):
+     pass 
+    def min_temp_rango(fecha1,fecha2):
+     pass 
+    def temp_extremos_rango(fecha1,fecha2):
+     pass
+    def borrar_temperatura(fecha):
+      pass 
+    def devolver_temperaturas(fecha1,fecha2): 
+      pass 
+    def cantidad_muestras(): 
+      pass  
